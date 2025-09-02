12a74cc0724edc660ed3b135bda056d5e10033d2
AnthonyCastro <antoniokevin307@gmail.com>
2025-09-02 11:33:48 -0600

    aparatos.py

--- aparatos.py
+++ aparatos.py
@@ -1,11 +1,12 @@
 class Aparato:
     def __init__(self, nombre, potencia, horas):
-        self.nombre = nombre
-        self.potencia = potencia
-        self.horas = horas
+        self.nombre = nombre        # Nombre del aparato
+        self.potencia = potencia    # Potencia en vatios
+        self.horas = horas          # Horas de uso al día
 
     def calcular_consumo(self):
-        return self.potencia * self.horas / 1000  # kWh
+        return self.potencia * self.horas / 1000  # Consumo en kWh
 
     def calcular_costo(self, precio_kwh):
-        return self.calcular_consumo() * precio_kwh
+        return self.calcular_consumo() * precio_kwh  # Costo según el precio de la energía
+
